5c5a88bfb8b39ccea74a17d5121c9fee4eae12ca
David Tamayo <67129995+DAVIDTS0286@users.noreply.github.com>
2022-06-18 11:58:31 -0500

    Primer commit: Estructura General Proyecto
    En este avance se presenta parte de la estructura general del proyecto en la cual se incluye un servidor local echo con nodejs para poder manejarlo de manera local

---
+++ src/app.js
@@ -0,0 +1,19 @@
+'use restrict'
+
+const express = require('express')
+
+const app = express()
+
+require('../drivers/mongo-connect')
+
+//settigs 
+app.set('port', process.emitWarning.PORT || 3000)
+
+//middleware
+app.use(express.json())
+app.use(express.urlencoded({extended:true}))
+
+//routers
+app.use('/api',require('../routes/index'));
+
+module.exports = app
